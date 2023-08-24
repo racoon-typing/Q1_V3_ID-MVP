56bacae47fa9e042bbc7fa033fda34b92e1f58b1
Semen Prozov <justpr0z.com.ru@gmail.com>
2023-08-24 15:43:05 +0300

    added accordions, lists and table

---
+++ src/utils/validatorNumber.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+  var phones = document.querySelectorAll("input[name=phone]");
+  for (var i = phones.length - 1; i >= 0; i--) {
+    processInput(phones[i]);
+  }
+});
+
+function processInput(phone) {
+  var iti = window.intlTelInput(phone, {
+    allowDropdown: false,
+    initialCountry: "GR",
+    nationalMode: true,
+    autoPlaceholder: "aggressive",
+    formatOnDisplay: true,
+    separateDialCode: false,
+    utilsScript:
+      "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.15/js/utils.min.js",
+  });
+  phone.addEventListener("input", () => {
+    phone.setCustomValidity("");
+    if (!iti.isValidNumber()) phone.setCustomValidity("!!!");
+  });
+}
