f946ac8e5f3d3e6fb4d7364508ea62352db7b3aa
Семен Прозов <justpr0z.com.ru@gmail.com>
2023-09-13 14:40:04 +0300

    added localStorage functions

---
+++ src/mock/text-2.js
@@ -0,0 +1,31 @@
+// Индивидуальный текст 2
+export const secondIndividualText = [
+  {
+    id: 1,
+    headerText:
+      '15 ribu orang dengan profil yang sama telah menurunkan berat badan melalui program penurunan berat badan dengan (PRODUCT)! Salah satunya adalah Titi DJ.',
+    name: 'Titi DJ',
+    text: 'Berkat program penurunan berat badan dari Dr. Cissie Nugraha, saya terlihat luar biasa! Anda bisa berdebat dengan saya, tapi menurut saya dia adalah ahli gizi terbaik di Indonesia. Dialah yang menyusun rencana penurunan berat badan untuk saya dan menyarankan suplemen bernama (PRODUCT). Teman-teman, saya telah mencoba banyak cara untuk menurunkan berat badan, tapi (PRODUCT) adalah sesuatu yang luar biasa. Saya akan jujur, saya tidak sepenuhnya mengikuti rencana latihan dan pola makan, dan saya pikir saya tidak akan berhasil. Namun, dalam sebulan saya berhasil menurunkan berat badan dan merasa penuh energi dan sehat! Jika Anda telah berjuang dengan berat badan berlebih untuk waktu yang lama, saya sangat menyarankan Anda mencoba (PRODUCT). Terima kasih, Dr. Cissie Nugraha, Anda adalah seorang profesional sejati!',
+  },
+  {
+    id: 2,
+    headerText:
+      '15 ribu orang dengan profil yang sama telah menurunkan berat badan melalui program penurunan berat badan dengan (PRODUCT)! Salah satunya adalah Audy Item.',
+    name: 'Audy Item',
+    text: 'Beberapa bulan yang lalu, saya menghubungi ahli gizi Cissie Nugraha dengan permintaan untuk membuat rencana penurunan berat badan. Kami bersama-sama menghitung parameter saya dan dia menyarankan saya untuk menggunakan produk bernama (PRODUCT). Saya mulai menggunakan (PRODUCT) dengan sedikit skeptis, karena saya memiliki pengalaman dengan produk penurunan berat badan lainnya. Namun, (PRODUCT) ternyata menjadi kejutan yang menyenangkan... Hanya dalam beberapa minggu penggunaan, saya berhasil mencapai berat badan target! Banyak dari kalian yang menyadari perubahan saya :) Salah satu keunggulan terbaik dari produk ini adalah tidak memerlukan pembatasan makanan atau latihan. Saya suka bahwa saya dapat menjalani hidup sehari-hari dan pada saat yang sama secara bertahap menurunkan berat badan. Jika Anda mencari cara untuk memulai perjalanan menuju gaya hidup sehat, (PRODUCT) adalah pilihan terbaik. Terima kasih, dokter Cissie Nugraha!',
+  },
+  {
+    id: 3,
+    headerText:
+      '15 ribu orang dengan profil yang sama telah menurunkan berat badan melalui program penurunan berat badan dengan (PRODUCT)! Salah satunya adalah Zaskia Gotik.',
+    name: 'Zaskia Gotik',
+    text: 'Setelah kehamilan, saya lama berjuang melawan kelebihan berat badan, dan itu sangat sulit. Meskipun saya berusaha keras, berat badan tidak mau berkurang. Namun, ketika saya mencoba (PRODUCT) berdasarkan rekomendasi dari ahli gizi saya, Cissie Nugraha, kondisi saya mulai berubah. Itu seperti penyelamatan bagi saya! Produk ini membantu saya perlahan dan tanpa stres untuk mengurangi berat badan setelah kehamilan. Saya mulai merasa lebih sehat dan aktif, seperti dulu. Salah satu hal yang saya hargai dari (PRODUCT) adalah cara kerjanya yang lembut pada tubuh saya. Saya tidak merasa lapar atau kekurangan energi. Saya bisa merawat bayi saya dan diri saya sendiri secara bersamaan. Jika Anda, seperti saya, menghadapi masalah kelebihan berat badan, saya sangat merekomendasikan (PRODUCT)!',
+  },
+  {
+    id: 4,
+    headerText:
+      '15 ribu orang dengan profil yang sama telah menurunkan berat badan melalui program penurunan berat badan dengan (PRODUCT)! Salah satunya adalah Agnez Mo.',
+    name: 'Agnez Mo',
+    text: 'Pekerjaan saya selalu diutamakan dan karena itu saya tidak punya waktu untuk berolahraga atau menjaga pola makan. Saya menyadari bahwa berat badan saya yang berlebih membuat saya tidak percaya diri, tapi saya tidak tahu bagaimana cara mengatasi masalah ini. Semua itu berubah ketika saya menghubungi Cissie Nugraha. Dia merekomendasikan saya (PRODUCT) dan itu benar-benar membuka mata saya. Produk ini telah menjadi penyelamat bagi kesehatan dan kesejahteraan saya. Yang membuat saya kagum adalah bahwa (PRODUCT) tidak membutuhkan banyak waktu dan usaha dari saya. Saya dapat melanjutkan pekerjaan saya dan (PRODUCT) menjaga berat badan saya. Saya mulai kehilangan berat badan dan saya merasa lebih baik dari sebelumnya. Jika Anda memiliki kesibukan seperti saya dan sedang mencari cara untuk menghilangkan kelebihan berat badan, (PRODUCT) mungkin bisa menjadi solusi Anda. Jangan menyangkal kesempatan ini untuk menuju kesehatan dan kenyamanan!',
+  },
+];
