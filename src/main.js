8dacb811ccade7ba46efe4f3ee1160177475bd69
Daniil <eo18622@yandex.ru>
2023-09-25 16:38:46 +0300

    add lazyloading plugin

--- src/main.js
+++ src/main.js
@@ -7,3 +7,7 @@ import './js/index.js';
 
 // bootstrap
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+// LazyLoadImg
+import 'lazysizes';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
